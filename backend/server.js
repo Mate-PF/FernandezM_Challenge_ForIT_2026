11d1d12c659cbc9b1405af84f283c05214afef45
Mate-PF <mateopaulofernandez173@.com>
2026-05-08 16:46:23 -0300

    cambio de http nativo a express

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
-import http from 'http'
+import express from 'express'
 
-const hostname = '127.0.0.1'
-const port = 3000
+const app = express()
+const PORT = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hola Mundo\n')
+app.get('/', (req, res) => {
+  res.send('Hola Mundo')
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Servidor corriendo en http://${hostname}:${port}`)
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })

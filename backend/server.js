f626d299843ebb37726125413cc9bf69b3ad444f
Mate-PF <mateopaulofernandez173@.com>
2026-05-08 17:04:48 -0300

    separar lógica de app y punto de entrada en server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,6 @@
-import express from 'express'
+import app from './app.js'
 
-const app = express()
 const PORT = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hola Mundo')
-})
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)

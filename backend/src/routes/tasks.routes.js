201b0fa0ff624628b80001a14aedfc4860e181d1
Mate-PF <mateopaulofernandez173@.com>
2026-05-08 19:53:27 -0300

    Implementar controladores updateTask y deleteTask + rutas CRUD completas

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -1,15 +1,11 @@
 import { Router } from 'express'
-import { getTasks, createTask } from '../controllers/tasks.controller.js'
+import { getTasks, createTask, updateTask, deleteTask } from '../controllers/tasks.controller.js'
 
 const router = Router()
 
 router.get('/', getTasks)
 router.post('/', createTask)
-router.put('/:id', (req, res) => {
-  res.json({ message: `PUT task ${req.params.id}` })
-})
-router.delete('/:id', (req, res) => {
-  res.json({ message: `DELETE task ${req.params.id}` })
-})
+router.put('/:id', updateTask)
+router.delete('/:id', deleteTask)
 
 export default router
